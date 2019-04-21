cf00d4eaedb598770ec6a00b60853f51c8600acd
GeorgCantor <cupsman1986@gmail.com>
2019-04-21 09:27:10 +0700

    refactored app

--- app/src/main/java/com/georgcantor/aac/util/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.georgcantor.aac.util
-
-/**
- * Created by Alex on 06.03.2018.
- */
-object Constants {
-
-    val BASE_URL = "https://jsonplaceholder.typicode.com"
-}
---
+++ app/src/main/java/com/georgcantor/aac/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.georgcantor.aac.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.georgcantor.aac.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
